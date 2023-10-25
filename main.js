9f6457974ed05668743f53cbf7e1520b1982a779
NOAH MORRISON <morrison.noah@gmail.com>
2023-10-25 14:48:09 -0700

    Hover effect for art- semi working

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
     function getRandomArtwork() {
         // Define a random page number to request different results each time
         const randomPage = Math.floor(Math.random() * 10) + 1;
-
+        const totalPages = 5;
+        const resultsPerPage = 100;
         // Make an API request
         $.ajax({
             url: apiEndpoint,
@@ -19,6 +20,7 @@ $(document).ready(function() {
                 q: '',
                 s: 'relevance',
                 p: randomPage, // Random page
+                ps: resultsPerPage,
             },
             success: function(data) {
                 // Check if there are any artworks in the response
@@ -29,8 +31,15 @@ $(document).ready(function() {
 
                     //art details
                     $('#artwork-image').attr('src', artwork.webImage.url);
+                    console.log("Applying artwork")
                     $('#artwork-title').text(artwork.title);
                     $('#artwork-artist').text(artwork.principalOrFirstMaker);
+                    if (artwork.plaqueDescription) {
+                        $('#artwork-description').text(artwork.plaqueDescription);
+                    } else {
+                        // No description found in the response
+                        $('#artwork-description').text('No description available');
+                    }
                 } else {
                     // No art found in the response
                     $('#artwork-image').attr('src', '');
@@ -44,6 +53,7 @@ $(document).ready(function() {
     $('#enter-button').on('click', function() {
         // Append the content from the hidden container to the artwork container
         $('#artwork-container').append($('#hidden-content').html());
+        
         // Trigger the getRandomArtwork function to fetch and display random artwork
         getRandomArtwork();
         // Hide the "Enter" button
@@ -51,7 +61,7 @@ $(document).ready(function() {
     });
     // Attach click event to the button
     $(document).on('click', '#get-random-artwork', getRandomArtwork);
-
+    console.log("Click detected on artwork button")
     // Get a random artwork when the page loads
     // getRandomArtwork();
 });
